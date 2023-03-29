9d0972ca960e14fa3accbc7d04d790ab3847092e
ikkrit <johan.car@protonmail.com>
2023-03-29 22:41:18 +0200

    second

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleProject01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "admin";
+            string password = "123";
+
+            if(username == "admin" && password == "123")
+            {
+                Console.WriteLine("Welcome admin");
+            } else
+            {
+                Console.WriteLine("Acces denied");
+            }
+            Console.Read();
+        }
+    }
+}
