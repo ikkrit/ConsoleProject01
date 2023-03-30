fc15c7aabcb295f6354f136e902feb39a5667f0d
ikkrit <johan.car@protonmail.com>
2023-03-30 22:37:45 +0200

    test

--- Program.cs
+++ Program.cs
@@ -6,8 +6,21 @@ namespace ConsoleProject01
     {
         static void Main(string[] args)
         {
-            int nb = Console.ReadLine();
+            /*string nb = Console.ReadLine();
             int resultat;
+
+            if(Int32.TryParse(nb, out resultat))
+            {
+                resultat = Int32.Parse(nb) * 2;
+                Console.WriteLine(resultat);
+            } else
+            {
+                Console.WriteLine("Error");
+            }*/
+
+            int nb = 33;
+            string str = nb.ToString();
+
 
             Console.Read();
         }
